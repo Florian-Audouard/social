509454f775017c62e2b4b18ade750939eb39d5b5
Florian-Audouard <nairolf.audouard@gmail.com>
2024-07-08 22:17:45 +0200

    vercel

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,22 @@
 import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-// import md5 from "md5";
+import md5 from "md5";
 
 const Login = () => {
 	const navigate = useNavigate();
-	// let url_add = "";
-	// if (process.env.NODE_ENV === "development") url_add = "http://localhost:80";
+	let url_add = "";
+	if (process.env.NODE_ENV === "development") url_add = "http://localhost:80";
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
-	// const [textConnection, setTextConnection] = useState("");
+	const [textConnection, setTextConnection] = useState("");
 
 	const input2 = useRef(null);
-	// function setCookie(cname, cvalue) {
-	// 	const date = new Date();
-	// 	date.setFullYear(date.getFullYear() + 100);
-	// 	let expires = "expires=" + date.toUTCString();
-	// 	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-	// }
+	function setCookie(cname, cvalue) {
+		const date = new Date();
+		date.setFullYear(date.getFullYear() + 100);
+		let expires = "expires=" + date.toUTCString();
+		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	}
 	const keyInputHandler = (event) => {
 		if (event.key !== "Enter") return;
 		switch (event.target.id) {
@@ -32,40 +32,40 @@ const Login = () => {
 	};
 	const logIn = () => {
 		console.log("ptdrr ?");
-		// setTextConnection("allo ?");
-		// if (username === "") {
-		// 	setTextConnection("Username can't be empty");
-		// 	return;
-		// }
-		// if (password === "") {
-		// 	setTextConnection("Password can't be empty");
-		// 	return;
-		// }
-		// fetch(url_add + "/LogIn", {
-		// 	method: "POST",
-		// 	body: JSON.stringify({ username, password: md5(password) }),
-		// })
-		// 	.then((res) => res.json())
-		// 	.then((data) => {
-		// 		if (data === "True") {
-		// 			setTextConnection("Connected");
-		// 			setCookie("username", username);
-		// 			setCookie("password", md5(password));
-		// 			setTimeout((_) => {
-		// 				navigate("/");
-		// 			}, 2000);
-		// 		} else {
-		// 			setTextConnection("Invalid username or password");
-		// 		}
-		// 	});
+		setTextConnection("allo ?");
+		if (username === "") {
+			setTextConnection("Username can't be empty");
+			return;
+		}
+		if (password === "") {
+			setTextConnection("Password can't be empty");
+			return;
+		}
+		fetch(url_add + "/LogIn", {
+			method: "POST",
+			body: JSON.stringify({ username, password: md5(password) }),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data === "True") {
+					setTextConnection("Connected");
+					setCookie("username", username);
+					setCookie("password", md5(password));
+					setTimeout((_) => {
+						navigate("/");
+					}, 2000);
+				} else {
+					setTextConnection("Invalid username or password");
+				}
+			});
 	};
-	// const showPassword = (event) => {
-	// 	if (event.target.checked) {
-	// 		input2.current.type = "text";
-	// 	} else {
-	// 		input2.current.type = "password";
-	// 	}
-	// };
+	const showPassword = (event) => {
+		if (event.target.checked) {
+			input2.current.type = "text";
+		} else {
+			input2.current.type = "password";
+		}
+	};
 	return (
 		<span className="login">
 			<div>Login :</div>
@@ -86,11 +86,11 @@ const Login = () => {
 				onKeyUp={keyInputHandler}
 			/>
 			<br></br>
-			{/* <input type="checkbox" onChange={showPassword} /> Show password */}
+			<input type="checkbox" onChange={showPassword} /> Show password
 			<br></br>
 			<button onClick={logIn}>Log in</button>
 			<button onClick={(_) => navigate("/signin")}>Sign in</button>
-			{/* <div>{textConnection}</div> */}
+			<div>{textConnection}</div>
 		</span>
 	);
 };
