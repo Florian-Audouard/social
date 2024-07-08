a7727e3a66795f94c359d43dee134d7758a6e3da
Florian-Audouard <nairolf.audouard@gmail.com>
2024-07-08 22:21:49 +0200

    vercel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Post from "../components/Post";
 // import Feed from "../components/Feed";
 import Account from "../components/Account";
 
 const Home = () => {
 	const [isLogIn, setIsLogIn] = useState(false);
-	const [username, setUsername] = useState("");
-	function getCookie(cname) {
-		let name = cname + "=";
-		let decodedCookie = decodeURIComponent(document.cookie);
-		let ca = decodedCookie.split(";");
-		for (let i = 0; i < ca.length; i++) {
-			let c = ca[i];
-			while (c.charAt(0) === " ") {
-				c = c.substring(1);
-			}
-			if (c.indexOf(name) === 0) {
-				return c.substring(name.length, c.length);
-			}
-		}
-		return "";
-	}
-	useEffect(() => {
-		let localUsername = getCookie("username");
-		let localPassword = getCookie("password");
-		if (
-			localUsername === "" ||
-			localPassword === "" ||
-			localPassword === undefined ||
-			localUsername === undefined
-		) {
-			setIsLogIn(false);
-			return;
-		}
-		setIsLogIn(true);
-		setUsername(localUsername);
-	}, []);
+	// const [username, setUsername] = useState("");
+	// function getCookie(cname) {
+	// 	let name = cname + "=";
+	// 	let decodedCookie = decodeURIComponent(document.cookie);
+	// 	let ca = decodedCookie.split(";");
+	// 	for (let i = 0; i < ca.length; i++) {
+	// 		let c = ca[i];
+	// 		while (c.charAt(0) === " ") {
+	// 			c = c.substring(1);
+	// 		}
+	// 		if (c.indexOf(name) === 0) {
+	// 			return c.substring(name.length, c.length);
+	// 		}
+	// 	}
+	// 	return "";
+	// }
+	// useEffect(() => {
+	// 	let localUsername = getCookie("username");
+	// 	let localPassword = getCookie("password");
+	// 	if (
+	// 		localUsername === "" ||
+	// 		localPassword === "" ||
+	// 		localPassword === undefined ||
+	// 		localUsername === undefined
+	// 	) {
+	// 		setIsLogIn(false);
+	// 		return;
+	// 	}
+	// 	setIsLogIn(true);
+	// 	setUsername(localUsername);
+	// }, []);
 	return (
 		<span className="Home">
 			<Account
-				isLogIn={isLogIn}
+				isLogIn={false}
 				setIsLogIn={setIsLogIn}
-				username={username}
+				username={""}
 			></Account>
-			{isLogIn ? <Post username={username}></Post> : <span></span>}
+			{isLogIn ? <Post username={""}></Post> : <span></span>}
 		</span>
 	);
 };
